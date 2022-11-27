fe61a7885eeefed1df9572a3337104607d172605
AhSenpai169 <97977304+AhSenpai169@users.noreply.github.com>
2022-11-27 23:17:02 +0200

    Create 3-Program-Сlasses-Example.js

---
+++ 3-SoftwareConstruction/3-Program-Сlasses-Example.js
@@ -0,0 +1,21 @@
+class Payment system {
+	constructor(name) {
+		this.name = name;
+	}
+}
+
+class Section {
+	constructor(type) {
+		this.type = type;
+	}
+}
+
+
+class Work {
+	constructor(name, type, description, price) {
+		this.name = name;
+		this.type = type;
+		this.description = description;
+		this.price = price;
+	}
+}
