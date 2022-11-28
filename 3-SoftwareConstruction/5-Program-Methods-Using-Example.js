352f4eaa20f52512a5954ed5308e17dce0d9250b
AhSenpai169 <97977304+AhSenpai169@users.noreply.github.com>
2022-11-28 02:05:57 +0200

    Update 5-Program-Methods-Using-Example.js

--- 3-SoftwareConstruction/5-Program-Methods-Using-Example.js
+++ 3-SoftwareConstruction/5-Program-Methods-Using-Example.js
@@ -4,8 +4,8 @@ user_3 = new User("jessepinkman@gmail.com", "12344567", "Capn Cook")
 
 work_array = [];
 
-work_array.append(user1.enter_work_info("FontName", "font", "Font for sale", 3));
-work_array.append(user2.enter_work_info("PictureName", "picture", "Picture for sale", 5));
-work_array.append(user3.enter_work_info("IconName", "icon", "Icon for sale", 2));
-user2.choose_work("IconName");
-user3.choose_work("FontName");
+work_array.append(user_1.enter_work_info("FontName", "font", "Font for sale", 3));
+work_array.append(user_2.enter_work_info("PictureName", "picture", "Picture for sale", 5));
+work_array.append(user_3.enter_work_info("IconName", "icon", "Icon for sale", 2));
+user_2.choose_work("IconName");
+user_3.choose_work("FontName");
